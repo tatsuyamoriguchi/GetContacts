46fead741083d94ea22308d36602ae25654a7cfb
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-10-08 15:29:57 -0700

    fetchSpecificContacts(phone: String) async { Refactor phoneEmailExtract

--- GetContacts/ContentView.swift
+++ GetContacts/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import Contacts
 
 struct ContentView: View {
+    
+    let phone = "(408) 555-3514" // (213) 555-5678"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,7 +22,8 @@ struct ContentView: View {
         .padding()
         .onAppear {
             Task.init {
-                await fetchAllContacts()
+//                await fetchAllContacts()
+                await fetchSpecificContacts(phone: phone)
             }
         }
     }
@@ -39,7 +43,6 @@ struct ContentView: View {
         do {
             try store.enumerateContacts(with: fetchRequest, usingBlock: { contact, result in
                 // Do something with contact
-                print("")
                 let name = contact.givenName + " " + contact.familyName
                 print(name)
                 if contact.organizationName != "" {
@@ -47,61 +50,102 @@ struct ContentView: View {
                 } else {
                     print("No organization name found")
                 }
-                for number in contact.phoneNumbers {
-                    
-                    switch number.label {
-                    case CNLabelPhoneNumberMain:
-                        print("- Main: \(number.value.stringValue)")
-                    case CNLabelPhoneNumberMobile:
-                        print("- Mobile: \(number.value.stringValue)")
-                    case CNLabelWork:
-                        print("- Work: \(number.value.stringValue)")
-                    case CNLabelHome:
-                        print("- Home: \(number.value.stringValue)")
-                    case CNLabelPhoneNumberPager:
-                        print("- Pager: \(number.value.stringValue)")
-                    case CNLabelSchool:
-                        print("- School: \(number.value.stringValue)")
-                    case CNLabelPhoneNumberAppleWatch:
-                        print("- Apple Watch: \(number.value.stringValue)")
-                    case CNLabelPhoneNumberHomeFax:
-                        print("- Home Fax: \(number.value.stringValue)")
-                    case CNLabelPhoneNumberWorkFax:
-                        print("- Work Fax: \(number.value.stringValue)")
-                    case CNLabelPhoneNumberOtherFax:
-                        print("- Other Fax: \(number.value.stringValue)")
-                    case CNLabelOther:
-                        print("- Other: \(number.value.stringValue)")
-                    default:
-                        print("- Unclassified Number: \(number.value.stringValue)")
-                    }
-                    
-                }
-//                print(contact.emailAddresses)
-                for email in contact.emailAddresses {
-                    switch email.label {
-                    case CNLabelEmailiCloud:
-                        print("- Cloud eMail: \(email.value)")
-                    case CNLabelWork:
-                        print("- Work: \(email.value)")
-                    case CNLabelHome:
-                        print("- Home: \(email.value)")
-                    case CNLabelSchool:
-                        print("- School: \(email.value)")
-                    case CNLabelOther:
-                        print("- Other: \(email.value)")
-                    default:
-                        print("- \(email.label ?? "Unclassified eMail"): \(email.value)")
 
-                    }
-                }
-
-            
+                phoneEmailExtract(contact)
+                print("")
             })
         } catch {
             print("Error")
         }
     }
+    
+    func fetchSpecificContacts(phone: String) async {
+        // Run this in the background
+        
+        // Get access to the Contacts store
+        let store = CNContactStore()
+        
+        let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactOrganizationNameKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey] as [CNKeyDescriptor]
+        
+        let phoneToFetch = CNPhoneNumber(stringValue: phone)
+        let predicate = CNContact.predicateForContacts(matching: phoneToFetch)
+        
+        do {
+            let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keys)
+            
+            
+            for contact in contacts {
+            
+                let name = contact.givenName + " " + contact.familyName
+                print(name)
+                if contact.organizationName != "" {
+                    print(contact.organizationName)
+                } else {
+                    print("No organization name found")
+                }
+
+                phoneEmailExtract(contact)
+            }
+
+        } catch {
+            print("Error")
+        }
+    }
+    
+    
+    fileprivate func phoneEmailExtract(_ contact: CNContact) {
+        
+        for number in contact.phoneNumbers {
+            
+            switch number.label {
+            case CNLabelPhoneNumberMain:
+                print("- Main: \(number.value.stringValue)")
+            case CNLabelPhoneNumberMobile:
+                print("- Mobile: \(number.value.stringValue)")
+            case CNLabelWork:
+                print("- Work: \(number.value.stringValue)")
+            case CNLabelHome:
+                print("- Home: \(number.value.stringValue)")
+            case CNLabelPhoneNumberPager:
+                print("- Pager: \(number.value.stringValue)")
+            case CNLabelSchool:
+                print("- School: \(number.value.stringValue)")
+            case CNLabelPhoneNumberAppleWatch:
+                print("- Apple Watch: \(number.value.stringValue)")
+            case CNLabelPhoneNumberHomeFax:
+                print("- Home Fax: \(number.value.stringValue)")
+            case CNLabelPhoneNumberWorkFax:
+                print("- Work Fax: \(number.value.stringValue)")
+            case CNLabelPhoneNumberOtherFax:
+                print("- Other Fax: \(number.value.stringValue)")
+            case CNLabelOther:
+                print("- Other: \(number.value.stringValue)")
+            default:
+                print("- Unclassified Number: \(number.value.stringValue)")
+            }
+            
+        }
+        
+        for email in contact.emailAddresses {
+            switch email.label {
+            case CNLabelEmailiCloud:
+                print("- Cloud eMail: \(email.value)")
+            case CNLabelWork:
+                print("- Work: \(email.value)")
+            case CNLabelHome:
+                print("- Home: \(email.value)")
+            case CNLabelSchool:
+                print("- School: \(email.value)")
+            case CNLabelOther:
+                print("- Other: \(email.value)")
+            default:
+                print("- \(email.label ?? "Unclassified eMail"): \(email.value)")
+                
+            }
+        }
+    }
+    
+
 }
 
 #Preview {
