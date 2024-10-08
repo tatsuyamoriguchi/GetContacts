0149ed067e75c678cb7656014059320630cff1f2
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-10-08 13:08:13 -0700

    Initial Commit

---
+++ GetContacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetContacts
+//
+//  Created by Tatsuya Moriguchi on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetContacts/GetContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetContactsApp.swift
+//  GetContacts
+//
+//  Created by Tatsuya Moriguchi on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GetContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
